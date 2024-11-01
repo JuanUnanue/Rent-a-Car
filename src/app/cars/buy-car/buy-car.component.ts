89fe436dd2fa913300b517f6cd5fb6ab09d97b8a
JuanUnanue <juanunanue99@gmail.com>
2024-11-01 19:28:57 -0300

    Buy Car component

---
+++ src/app/cars/buy-car/buy-car.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-car',
+  templateUrl: './buy-car.component.html',
+  styleUrl: './buy-car.component.css'
+})
+export class BuyCarComponent {
+
+}
--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddCarsComponent } from './add-cars/add-cars.component';
+import { BuyCarComponent } from './buy-car/buy-car.component';
 
 
 
 @NgModule({
   declarations: [
-    AddCarsComponent
+    AddCarsComponent,
+    BuyCarComponent
   ],
   imports: [
     CommonModule
